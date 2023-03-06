9f672a3899db138e3ff3d26b3e196dbb9e6b02b7
Sanghak Lee <yee950419@naver.com>
2023-03-06 15:13:33 +0900

    AppConfig 리팩토링

--- 스프링 핵심 원리 기본/core/src/main/java/com/study/core/AppConfig.java
+++ 스프링 핵심 원리 기본/core/src/main/java/com/study/core/AppConfig.java
@@ -2,6 +2,7 @@ package com.study.core;
 
 import com.study.core.discount.DiscountPolicy;
 import com.study.core.discount.RateDiscountPolicy;
+import com.study.core.member.MemberRepository;
 import com.study.core.member.MemberService;
 import com.study.core.member.MemberServiceImpl;
 import com.study.core.member.MemoryMemberRepository;
@@ -11,9 +12,18 @@ import com.study.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() {
-        return new OrderServiceImpl(memberService(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberService(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
