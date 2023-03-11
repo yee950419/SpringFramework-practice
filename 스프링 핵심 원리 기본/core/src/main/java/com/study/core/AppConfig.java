4258cba7687e0019829dc3828d139eabe01f7e3e
Sanghak Lee <yee950419@naver.com>
2023-03-11 11:25:29 +0900

    Map, List 형식으로 빈 조회 및 사용 test 코드

--- 스프링 핵심 원리 기본/core/src/main/java/com/study/core/AppConfig.java
+++ 스프링 핵심 원리 기본/core/src/main/java/com/study/core/AppConfig.java
@@ -1,36 +1,36 @@
-//package com.study.core;
-//
-//import com.study.core.discount.DiscountPolicy;
-//import com.study.core.discount.RateDiscountPolicy;
-//import com.study.core.member.MemberRepository;
-//import com.study.core.member.MemberService;
-//import com.study.core.member.MemberServiceImpl;
-//import com.study.core.member.MemoryMemberRepository;
-//import com.study.core.order.OrderService;
-//import com.study.core.order.OrderServiceImpl;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class AppConfig {
-//
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberServiceImpl(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
-//
-//    @Bean
-//    public OrderService orderService() {
-//        return new OrderServiceImpl(memberService(), discountPolicy());
-//    }
-//
-//    @Bean
-//    public DiscountPolicy discountPolicy() {
-//        return new RateDiscountPolicy();
-//    }
-//}
+package com.study.core;
+
+import com.study.core.discount.DiscountPolicy;
+import com.study.core.discount.RateDiscountPolicy;
+import com.study.core.member.MemberRepository;
+import com.study.core.member.MemberService;
+import com.study.core.member.MemberServiceImpl;
+import com.study.core.member.MemoryMemberRepository;
+import com.study.core.order.OrderService;
+import com.study.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberService(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
