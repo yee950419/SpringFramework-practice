d0aab3df04a2ba91fb5eed219c871351985d61aa
Sanghak Lee <yee950419@naver.com>
2023-03-25 12:33:44 +0900

    모델을 파라미터로 넘기고 뷰의 논리 이름을 반환하는 식으로 사용하기 쉽게 변경

--- 스프링 MVC/servlet/src/main/java/com/study/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
+++ 스프링 MVC/servlet/src/main/java/com/study/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -1,23 +1,23 @@
-package com.study.servlet.web.frontcontroller.v3.controller;
+package com.study.servlet.web.frontcontroller.v4.controller;
 
 import com.study.servlet.domain.member.Member;
 import com.study.servlet.domain.member.MemberRepository;
 import com.study.servlet.web.frontcontroller.ModelView;
 import com.study.servlet.web.frontcontroller.v3.ControllerV3;
+import com.study.servlet.web.frontcontroller.v4.ControllerV4;
 
 import java.util.List;
 import java.util.Map;
 
-public class MemberListControllerV3 implements ControllerV3 {
+public class MemberListControllerV4 implements ControllerV4 {
 
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public ModelView process(Map<String, String> paramMap) {
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
         List<Member> members = memberRepository.findAll();
-        ModelView mv = new ModelView("member-list");
-        mv.getModel().put("memberList", members);
 
-        return mv;
+        model.put("memberList", members);
+        return "member-list";
     }
 }
