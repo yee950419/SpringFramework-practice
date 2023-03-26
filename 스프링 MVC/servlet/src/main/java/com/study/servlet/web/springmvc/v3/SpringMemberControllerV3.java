a5d111d2cf3d298d35fcda6830a11f46abaee8fd
Sanghak Lee <yee950419@naver.com>
2023-03-26 13:08:57 +0900

    spring mvc로 코드 변경

---
+++ 스프링 MVC/servlet/src/main/java/com/study/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,41 @@
+package com.study.servlet.web.springmvc.v3;
+
+import com.study.servlet.domain.member.Member;
+import com.study.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        model.addAttribute("member", member);
+
+        return "save-result";
+    }
+
+    @GetMapping
+    public String showList(Model model) {
+        List<Member> memberList = memberRepository.findAll();
+        model.addAttribute("memberList", memberList);
+        return "member-list";
+    }
+}
