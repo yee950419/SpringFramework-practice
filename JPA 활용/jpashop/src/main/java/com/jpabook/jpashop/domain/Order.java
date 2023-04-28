7d65bf22ea119a8089744706ab12edc6ee175c5a
Sanghak Lee <yee950419@naver.com>
2023-04-28 14:41:48 +0900

    주문, 주문상품 엔티티 개발

--- JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
+++ JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,7 @@ package com.jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -48,4 +49,42 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem ... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for(OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    public void cancle() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setOrderStatus(OrderStatus.CANCLE);
+        for(OrderItem orderItem : orderItems) {
+            orderItem.cancle();
+        }
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
+
 }
