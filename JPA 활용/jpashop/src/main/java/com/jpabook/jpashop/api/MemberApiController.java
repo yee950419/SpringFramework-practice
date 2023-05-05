568f356c64f06e6c27785fc62c9a734be387b6d7
Sanghak Lee <yee950419@naver.com>
2023-05-05 12:56:33 +0900

    회원 조회 API

--- JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -11,12 +11,46 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    //return 값이 배열로 감싸져 반환되므로 확장이 어려움
+    //Entity가 변경되면 api 스팩이 변함
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    //data로 감싼 뒤, MemberDto로 필요한 값만 정의해서 반환
+    //Entity가 변경되도 api 스팩이 변하지 않음
+    @GetMapping("/api/v2/members")
+    public Result nembersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName(), m.getAddress()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+        private Address address;
+    }
 
     //외부로 엔티티를 직접 노출시키는 것은 절대 X
     @PostMapping("/api/v1/members")
