a302d256e3c4663c83a49faac9f89a0e01507b41
Sanghak Lee <yee950419@naver.com>
2023-05-06 23:46:25 +0900

    주문 조회 API V1(절대 하면 안되는 방법)

---
+++ JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,38 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * XToOne 관계(ManyToOne, OneToOne)
+ *
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+
+        //Lazy 강제 초기화
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
+
+        return all;
+    }
+
+}
