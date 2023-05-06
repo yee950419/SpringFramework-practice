f2860792cc0052d5a83913f79d2d22296df45701
Sanghak Lee <yee950419@naver.com>
2023-05-07 00:34:09 +0900

    주문 조회 API V2(여전히 N+1 문제 남아있음)

--- JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
+++ JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,17 +1,23 @@
 package com.jpabook.jpashop.api;
 
+import com.jpabook.jpashop.domain.Address;
 import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderStatus;
 import com.jpabook.jpashop.repository.OrderRepository;
 import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * XToOne 관계(ManyToOne, OneToOne)
- *
+ * <p>
  * Order
  * Order -> Member
  * Order -> Delivery
@@ -31,8 +37,37 @@ public class OrderSimpleApiController {
             order.getMember().getName();
             order.getDelivery().getAddress();
         }
-
         return all;
     }
 
+    @GetMapping("/api/v2/simple-orders")
+    public OrderResult orderV2() {
+
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> collect = orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        return new OrderResult(collect);
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class OrderResult<T>{
+        private T orderData;
+    }
 }
