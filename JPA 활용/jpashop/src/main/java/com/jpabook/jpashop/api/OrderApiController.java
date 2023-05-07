58334b77d66e8439f68d17cfea8fed373a7a6d80
Sanghak Lee <yee950419@naver.com>
2023-05-07 14:39:41 +0900

    1:M 관계 주문 조회 API V1(이렇게 하면 무조건 X)

---
+++ JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,34 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            orderItems.stream().map(o -> o.getItem().getName());
+//            for (OrderItem orderItem : orderItems) {
+//                orderItem.getItem().getName();
+//            }
+        }
+        return all;
+    }
+}
