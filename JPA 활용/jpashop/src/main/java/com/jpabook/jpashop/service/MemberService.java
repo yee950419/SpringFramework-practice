94f581ff92b48937c9ecc5c6db66ea2da8d0f0f5
Sanghak Lee <yee950419@naver.com>
2023-05-09 01:24:04 +0900

    Spring Data Jpa 적용

--- JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/service/MemberService.java
+++ JPA 활용/jpashop/src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -25,7 +25,7 @@ public class MemberService {
     }
 
     public Member findOne(Long id) {
-        return memberRepository.findOne(id);
+        return memberRepository.findById(id).get();
     }
 
     public List<Member> findMembers() {
@@ -34,7 +34,7 @@ public class MemberService {
 
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 
