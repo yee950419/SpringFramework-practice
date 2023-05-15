4ba39ac35d7e5577047f31bef33ecfbb6046a0e4
Sanghak Lee <yee950419@naver.com>
2023-05-15 17:57:45 +0900

    데이터 중심 엔티티 설계 -> 연관관계 설정 안됨

--- JPA 기본/basic/src/main/java/hellojpa/JpaMain.java
+++ JPA 기본/basic/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,8 @@
 package hellojpa;
 
+import hellojpa.domain.Member;
+
 import javax.persistence.*;
-import java.util.List;
 
 public class JpaMain {
 
@@ -12,46 +13,9 @@ public class JpaMain {
 
         tx.begin();
 
-        //등록
         try{
-            Member member = new Member();
-            member.setId(3L);
-            member.setName("sanghak");
-
-            em.persist(member);
 
             tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-//            em.close();
-        }
-
-
-        //수정
-        try{
-            Member member = em.find(Member.class, 1L);
-            member.setName("joohak");
-
-            tx.commit();
-        } catch (Exception e){
-            tx.rollback();
-        } finally {
-//            em.close();
-        }
-
-        emf.close();
-
-        //전체 조회
-        try{
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
-                    .getResultList();
-
-            for (Member member : members) {
-                System.out.println(member.getName());
-            }
         } catch (Exception e) {
             tx.rollback();
         } finally {
